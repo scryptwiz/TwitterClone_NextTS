b3c04af0cd0ed9fb94b6dcc26796144bfef4544c
Kelvin Ajayi <68742854+Kelvide@users.noreply.github.com>
2022-08-06 14:20:42 +0100

    Update sanity.js

--- sanity.js
+++ sanity.js
@@ -4,7 +4,8 @@ export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion: '2021-10-21',
-    useCdn: process.env.NODE_ENV === 'production',
+    useCdn: true,
+    revalidate: 1,
   }
 
   export const sanityClient = createClient(config)
