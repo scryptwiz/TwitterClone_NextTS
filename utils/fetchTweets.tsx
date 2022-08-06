49fe14335ef677e6a8b3e943177230bd120c93ed
Kelvin Ajayi <68742854+Kelvide@users.noreply.github.com>
2022-08-06 12:59:15 +0100

    await

--- utils/fetchTweets.tsx
+++ utils/fetchTweets.tsx
@@ -3,6 +3,6 @@ import { Tweet } from "../typings";
 export const fetchTweets = async () =>{
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets`)
     const data  =  await res.json();
-    const tweets: Tweet[] = data.tweets;
-    return tweets
+    const tweets: Tweet[] = await data.tweets;
+    return tweets;
 }
